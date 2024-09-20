388ebfb3f56fea57656e68c2f959521cf6811990
bylaye <abdoulayeniang2010@gmail.com>
2024-09-20 19:32:58 +0200

    all teams grouped by pots with 4 pots of 9 teams

---
+++ teams.py
@@ -0,0 +1,50 @@
+pots = {
+        'pot1': [
+            ('Borussia Dortmund', '(GER)'), 
+            ('Barcelona', '(ESP)'), 
+            ('Bayern München', '(GER)'), 
+            ('Inter', '(ITA)'), 
+            ('RB Leipzig', '(GER)'), 
+            ('Liverpool', '(ENG)'), 
+            ('Manchester City', '(ENG)'), 
+            ('Paris Saint-Germain', '(FRA)'), 
+            ('Real Madrid', '(ESP)')
+        ], 
+
+        'pot2': [
+            ('Arsenal', '(ENG)'), 
+            ('Atalanta', '(ITA)'), 
+            ('Atlético de Madrid', '(ESP)'), 
+            ('Benfica', '(POR)'), 
+            ('Club Brugge', '(BEL)'), 
+            ('Juventus', '(ITA)'), 
+            ('Bayer 04 Leverkusen', '(GER)'), 
+            ('AC Milan', '(ITA)'), 
+            ('FC Shakhtar Donetsk', '(UKR)')
+        ],
+
+       'pot3': [
+            ('Crvena Zvezda', '(SRB)'), 
+            ('Celtic FC', '(SCO)'),
+            ('Feyenoord', '(NED)'), 
+            ('GNK Dinamo', '(CRO)'), 
+            ('LOSC Lille', '(FRA)'), 
+            ('PSV Eindhoven', '(NED)'), 
+            ('Salzburg', '(AUT)'), 
+            ('Sporting CP', '(POR)'), 
+            ('Young Boys', '(SUI)')
+        ],
+
+        'pot4': [
+            ('Aston Villa', '(ENG)'), 
+            ('Bologna', '(ITA)'), 
+            ('Stade Brestois 29', '(FRA)'), 
+            ('Girona', '(ESP)'), 
+            ('AS Monaco', '(FRA)'), 
+            ('ŠK Slovan Bratislava', '(SVK)'), 
+            ('Sparta Praha', '(CZE)'), 
+            ('Sturm Graz', '(AUT)'), 
+            ('Stuttgart', '(GER)')
+        ],
+
+}
